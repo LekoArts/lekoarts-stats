1479854dccf3ccfcbff100dec2aef51c90597eab
LekoArts <lekoarts@gmail.com>
2022-10-15 18:03:10 +0200

    fix unit tests

--- src/utils/__tests__/normalize.ts
+++ src/utils/__tests__/normalize.ts
@@ -1,62 +1,22 @@
-import { constructShape, flatten, nivoGithubFormatter, nivoTwitterFormatter, normalizeGithub } from '../normalize'
+import { constructShape, nivoGithubFormatter, nivoTwitterFormatter, normalizeGithub } from '../normalize'
 import { data as githubData } from './__fixtures__/github'
 import { data as twitterData } from './__fixtures__/twitter'
 
-describe('flatten', () => {
-  it('should create new object shape', () => {
-    const flat = flatten(githubData)
-
-    const objectShape = [
-      {
-        datetime: '2020-07-17',
-        forks: 154,
-        name: 'gatsby-starter-minimal-blog',
-        stars: 517,
-        url: 'https://github.com/LekoArts/gatsby-starter-minimal-blog',
-      },
-    ]
-
-    expect(flat).toMatchSnapshot()
-    expect(flat).toEqual(expect.arrayContaining(objectShape))
-    expect(flat[0].id).not.toBeDefined()
-  })
-  it('should create entries in correct order', () => {
-    const flat = flatten(githubData)
-    const length = flat.length
-
-    expect(flat[0]).toStrictEqual({
-      datetime: '2020-07-17',
-      forks: 243,
-      name: 'gatsby-themes',
-      stars: 745,
-      url: 'https://github.com/LekoArts/gatsby-themes',
-    })
-    expect(flat[length - 1]).toStrictEqual({
-      datetime: '2020-07-18',
-      forks: 18,
-      name: 'gatsby-starter-portfolio-bella',
-      stars: 113,
-      url: 'https://github.com/LekoArts/gatsby-starter-portfolio-bella',
-    })
-  })
-})
-
 describe('constructShape', () => {
   it('should group data by name and omit name', () => {
-    const flat = flatten(githubData)
-    const shape = constructShape(flat, 'name', 'name')
+    const shape = constructShape(githubData, 'name', 'name')
     const namedEntry = shape['gatsby-starter-minimal-blog']
 
     expect(shape).toMatchSnapshot()
     expect(namedEntry).toStrictEqual([
       {
-        datetime: '2020-07-17',
+        createdAt: '2020-07-17',
         forks: 154,
         stars: 517,
         url: 'https://github.com/LekoArts/gatsby-starter-minimal-blog',
       },
       {
-        datetime: '2020-07-18',
+        createdAt: '2020-07-18',
         forks: 153,
         stars: 517,
         url: 'https://github.com/LekoArts/gatsby-starter-minimal-blog',
@@ -72,13 +32,13 @@ describe('normalizeGithub', () => {
 
     expect(namedEntry).toStrictEqual([
       {
-        datetime: '2020-07-17',
+        createdAt: '2020-07-17',
         forks: 154,
         stars: 517,
         url: 'https://github.com/LekoArts/gatsby-starter-minimal-blog',
       },
       {
-        datetime: '2020-07-18',
+        createdAt: '2020-07-18',
         forks: 153,
         stars: 517,
         url: 'https://github.com/LekoArts/gatsby-starter-minimal-blog',
