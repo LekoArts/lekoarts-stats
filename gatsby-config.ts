d2a4120b86890a266c5d3cd6ffc47caff65514d8
LekoArts <lekoarts@gmail.com>
2024-01-06 12:31:25 +0100

    chore: Update metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,16 +4,16 @@ import 'dotenv/config'
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'LekoArts Statistics',
-    url: 'https://lekoartsstats.netlify.app/',
+    url: 'https://stats.lekoarts.de',
     repo: 'https://github.com/LekoArts/lekoarts-stats',
     github: 'https://www.github.com/LekoArts',
     twitter: 'https://www.twitter.com/lekoarts_de',
     mastodon: 'https://mastodon.social/@lekoarts',
     trakt: 'https://trakt.tv/users/arsaurea',
-    homepage: 'https://www.lekoarts.de',
+    homepage: 'https://www.lekoarts.de?utm_source=lekoarts-stats',
     image: '/social.png',
     author: '@lekoarts_de',
-    description: 'Dashboard of LekoArts\' GitHub and Twitter statistics over time, visualized with fancy graphs.',
+    description: 'Dashboard of LekoArts\' GitHub, Mastodon, Twitter, and Trakt.tv statistics over time, visualized with fancy graphs.',
   },
   plugins: ['gatsby-plugin-lodash', 'gatsby-plugin-vanilla-extract'],
 }
